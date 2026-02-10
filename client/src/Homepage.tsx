9d9b1548c28084aa8f504aa1b6d7d9a9df45d31c
mdestafadilah <desta.08b@gmail.com>
2026-02-10 13:57:34 +0700

    feat: Introduce homepage with URL shortening input form component.

--- client/src/Homepage.tsx
+++ client/src/Homepage.tsx
@@ -1,5 +1,5 @@
 import { Heading, Text, Link } from "@chakra-ui/react";
-import { InputForm } from "./components/InputForm";
+import { InputForm } from "./components/InputForm.tsx";
 
 export default function Homepage() {
     return (
@@ -9,9 +9,13 @@ export default function Homepage() {
             </Heading>
             <InputForm />
             <Text m={'3%'}>
-                Made with ❤️ by Me ({' '}
+                Based On ({' '}
                 <Link isExternal href="https://akshay-kumar-portfoilo.netlify.app/">
                     Akshay Kumar
+                </Link>
+                ) & Modified By ({' '}
+                <Link isExternal href="https://github.com/rsuppersahabatan/pendekin-url">
+                    M Desta Fadilah
                 </Link>
                 )
             </Text>
