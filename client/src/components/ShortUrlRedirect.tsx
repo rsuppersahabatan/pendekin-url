765e60772ae03ae4c0570f54b0c0743f30268e89
mdestafadilah <desta.08b@gmail.com>
2026-02-12 11:51:05 +0700

    Loading keliatan...

--- client/src/components/ShortUrlRedirect.tsx
+++ client/src/components/ShortUrlRedirect.tsx
@@ -1,25 +1,28 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, Text } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function ShortUrlRedirect() {
     const { urlCode } = useParams();
-    const redirect = () => {
-        let url = `/${urlCode}`;
-        window.location.replace(url);
-    };
 
     useEffect(() => {
         if (urlCode) {
-            redirect();
+            const timer = setTimeout(() => {
+                window.location.replace(`/${urlCode}`);
+            }, 3000);
+            return () => clearTimeout(timer);
         }
     }, [urlCode]);
 
     return (
-        <div style={{ textAlign: 'center', marginTop: '20%' }}>
-            <Heading as="h3" size="xl">
-                Loading...
+        <div className="loader-container">
+            <span className="loader"></span>
+            <Heading as="h3" size="lg" color="gray.600">
+                Redirecting...
             </Heading>
+            <Text color="gray.500">
+                Please wait while we take you to your destination.
+            </Text>
         </div>
     );
 }
